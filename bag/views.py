74cf52a31adfbc50918de8fed2478c6aa9d138f9
marijavelickovska <marija_velickovska88@hotmail.com>
2025-07-21 11:39:26 +0200

    Update context processor

--- bag/views.py
+++ bag/views.py
@@ -21,5 +21,5 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity  # add the item 
 
     request.session['bag'] = bag  # overwrite the variable in the session with the updated version
-    print(request.session['bag'])
+    # print(request.session['bag'])
     return redirect(redirect_url)
