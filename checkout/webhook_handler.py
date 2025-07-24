b63350b5d94602daec8e5dca34d8bbf4e8c1b684
marijavelickovska <marija_velickovska88@hotmail.com>
2025-07-24 16:09:08 +0200

    Add webhooks.py, settings config, and URL pattern for webhook handling

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -4,13 +4,28 @@ from django.http import HttpResponse
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
 
-    # The init method of the class is a setup method that's called every time an instance of the class is created.
     def __init__(self, request):
         self.request = request
-        
+
     def handle_event(self, event):
         """
         Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle the payment_intent.succeeded webhook from Stripe
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle the payment_intent.payment_failed webhook from Stripe
         """
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
