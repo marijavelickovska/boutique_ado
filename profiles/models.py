91f97a9763f0af17d4420bdc51b434e6730242b9
marijavelickovska <marija_velickovska88@hotmail.com>
2025-07-27 14:14:47 +0200

    Add UserProfileForm, update view and template

--- profiles/models.py
+++ profiles/models.py
@@ -11,14 +11,14 @@ class UserProfile(models.Model):
     A user profile model for maintaining default
     delivery information and order history
     """
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userprofile')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label='Country *', null=True, blank=True)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country', null=True, blank=True)
 
     def __str__(self):
         return self.user.username
@@ -33,6 +33,7 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(user=instance)
     # Existing users: just save the profile
     instance.userprofile.save()
+    
 
 
 # And let's also add a quick receiver for the post save event from the user model. 
--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    form = UserProfileForm(instance=profile)  # Populate it with the user's current profile information.
+    orders = profile.orders.all()  # orders e related name za Order modelot
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
     }
     
     return render(request, template, context)
