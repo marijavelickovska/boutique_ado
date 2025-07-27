8f2e13618c8958dd63106b0783cb6f0b5bbcb018
marijavelickovska <marija_velickovska88@hotmail.com>
2025-07-27 14:45:01 +0200

    Add style to UserProfileForm, add js and update view

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -8,12 +9,20 @@ def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
     form = UserProfileForm(instance=profile)  # Populate it with the user's current profile information.
     orders = profile.orders.all()  # orders e related name za Order modelot
 
     template = 'profiles/profile.html'
     context = {
         'form': form,
+        'orders': orders,
+        'on_profile_page': True  # varijabla sto ja koristime za da ne se prikazuva success message zaedno so bag, ako userot e na profile page
     }
-    
+
     return render(request, template, context)
